d90cdc1aa639330140b3c90b75fa7b20e847cc03
fetak-jap <jajsemdostdobrej@seznam.cz>
2025-09-09 17:32:21 +0200

    Logo a clear terminal

--- projekt_uno/Konzole.java
+++ projekt_uno/Konzole.java
@@ -1,5 +1,15 @@
 package projekt_uno;
 
 public class Konzole {
-    
+    public static void clearConsole() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (Exception e) {
+            System.out.println("Nepodařilo se vyčistit konzoli.");
+        }
+    }
 }
--- projekt_uno/Main.java
+++ projekt_uno/Main.java
@@ -1,5 +1,19 @@
 package projekt_uno;
 
 public class Main {
-    
+    public static void main(String[] args) {
+
+        String unoLogo = 
+            "##     ##  ##   ##   ###### \n" +
+            "##     ##  ###  ##  ##   ##\n" +
+            "##     ##  #### ##  ##   ##\n" +
+            "##     ##  ## ####  ##   ##\n" +
+            "##     ##  ##  ###  ##   ##\n" +
+            "##     ##  ##   ##  ##   ##\n" +
+            " #######   ##   ##   ######";
+        
+        System.out.println(unoLogo);
+
+    }
+
 }
