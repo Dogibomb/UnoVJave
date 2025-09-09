df71b134fa4cdcfd73214f6d9987979bb6ef9a88
fetak-jap <jajsemdostdobrej@seznam.cz>
2025-09-09 17:40:56 +0200

    rozdavani karet, psani a hlavni cykl

--- projekt_uno/Karty.java
+++ projekt_uno/Karty.java
@@ -1,5 +1,49 @@
 package projekt_uno;
 
+import java.util.Random;
+
 public class Karty {
     
+        Random rand = new Random();
+
+    String[] karty = {
+        "0" , "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    };
+    String[] specialniKarty = {
+    "2+", "4+"
+    };
+    String[] barvy = {
+        "M", "Z", "Cer", "Zl"
+    };
+
+    public String[] getKartyInHands() {
+        String[] KartyInHands = new String[7];
+        String[] barvyKaret = new String[7];
+        for(int i = 0; i<7; i++){
+
+            int randomBarvaNumber;
+            int randomKardNumber;
+            int specialniKarta;
+
+            if (rand.nextInt(100) < 50) {
+                randomKardNumber = (int)(Math.random() * karty.length);
+                randomBarvaNumber = (int)(Math.random() * barvy.length);
+                KartyInHands[i] = karty[randomKardNumber];
+                barvyKaret[i] = barvy[randomBarvaNumber];
+                KartyInHands[i] += barvyKaret[i];
+                
+            } 
+            else{
+                specialniKarta = (int)(Math.random() * specialniKarty.length);
+                randomBarvaNumber = (int)(Math.random() * barvy.length);
+                KartyInHands[i] = specialniKarty[specialniKarta] + barvy[randomBarvaNumber];
+            }
+
+
+        }
+        return KartyInHands;
+    }
+
+
+
 }
--- projekt_uno/Main.java
+++ projekt_uno/Main.java
@@ -1,7 +1,18 @@
 package projekt_uno;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Karty karty = new Karty();
+
+        int na_rade = 0;
+        
+        boolean hra = true;
+
 
         Konzole.clearConsole();
 
@@ -15,6 +26,40 @@ public class Main {
             " #######   ##   ##   ######";
         
         System.out.println(unoLogo);
+
+        String[] kartyHrace1 = karty.getKartyInHands();
+        String[] kartyHrace2 = karty.getKartyInHands();
+
+        ArrayList<String> kartyHrace1list = new ArrayList<String>(Arrays.asList(kartyHrace1));
+        ArrayList<String> kartyHrace2list = new ArrayList<String>(Arrays.asList(kartyHrace2));
+
+        String Jednakarta = karty.getOneKard() + karty.getOneColor();
+
+        
+
+
+        boolean hrac1 = true;
+        
+        Scanner scanner = new Scanner(System.in);
+
+
+        boolean kartaExistuje = false;
+
+        do{
+            
+            if (na_rade % 2 == 0) {
+                System.out.println("------------------------");
+                System.out.println("Hráč 1 je na řadě");
+                System.out.println(String.join(", ", kartyHrace1list));
+                System.out.println("------------------------");
+                hrac1 = true;
+            } else {
+                System.out.println("------------------------");
+                System.out.println("Hráč 2 je na řadě");
+                System.out.println(String.join(", ", kartyHrace2list));
+                System.out.println("------------------------");
+                hrac1 = false;
+            }
 
     }
 
